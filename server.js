85b1a3685ed8679718c6e24e35cc50bf8d872b75
olgaprylutska <prol22fl@student.ju.se>
2025-03-05 12:19:11 +0100

    changes

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express();
 ///port///
 const PORT = 3000;
 
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to the SQLite database (creates 'database.db' if it doesn't exist)
+const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        console.error('Error opening database:', err.message);
+    } else {
+        console.log('Connected to SQLite database.');
+    }
+});
 
 
 
