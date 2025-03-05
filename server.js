73698aa312f62643b42bf956b398cb442f587876
olgaprylutska <prol22fl@student.ju.se>
2025-03-05 12:12:27 +0100

    changes

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
+///application///
 const app = express();
+
+///port///
 const PORT = 3000;
 
+
+
+
+
+///middlewares ///
 app.use(express.static("public"));
 
+
+
+///routes///
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello world!" });
 });
 
+///listen///
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
+
+
